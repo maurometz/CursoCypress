10eddf8c353909a7913e02d1c515bfcb35087fa2
Mauro <maurometzdasilva@ienh.com.br>
2021-06-28 18:00:47 -0300

    Outras alterações muito interessantes

--- cypress/integration/testesApp/backend.spec.js
+++ cypress/integration/testesApp/backend.spec.js
@@ -32,16 +32,10 @@ describe('Testes com o Barriga React', () => {
     })
 
     it('Alterando conta', () => {
-        cy.request({
-            method: 'GET',
-            url: '/contas',
-            headers: { Authorization: `JWT ${token}` },
-            qs: {
-                nome: 'Conta para alterar'
-            }
-        }).then(res => {
+        cy.getContaByName('Conta para alterar')
+        .then(contaId => {
             cy.request({
-                url: `/contas/${res.body[0].id}`,
+                url: `/contas/${contaId}`,
                 method: 'PUT',
                 headers: { Authorization: `JWT ${token}` },
                 body: {
@@ -65,14 +59,32 @@ describe('Testes com o Barriga React', () => {
         }).as('response')
 
         cy.get('@response').then(res => {
-            console.log(res)
             expect(res.status).to.be.equal(400)
             expect(res.body.error).to.be.equal('Já existe uma conta com esse nome!')
         })
     })
 
     it('Deve criar uma transação', () => {
-
+        cy.getContaByName('Conta para movimentacoes')
+        .then(contaId => {
+            cy.request({
+                method: 'POST',
+                url: '/transacoes',
+                headers: { Authorization: `JWT ${token}` },
+                body: {
+                    conta_id: contaId,
+                    data_pagamento: Cypress.moment().add({days: 1}).format('DD/MM/YYYY'),
+                    data_transacao: Cypress.moment().format('DD/MM/YYYY'),
+                    descricao: "asdac",
+                    envolvido: "Min Winters",
+                    status: true,
+                    tipo: "REC",
+                    valor: "123"
+                }
+            }).as('response')
+        })
+        cy.get('@response').its('status').should('be.equal', 201)
+        cy.get('@response').its('body.id').should('exist')
     })
 
     it('Pegar o saldo', () => {
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -70,3 +70,18 @@ Cypress.Commands.add('resetRest', () => {
         }).its('status').should('be.equal', 200)
     })
 })
+
+Cypress.Commands.add('getContaByName', name => {
+    cy.getToken('ehre@ehre.com', '123456789').then(token => {
+        cy.request({
+            method: 'GET',
+            url: '/contas',
+            headers: { Authorization: `JWT ${token}` },
+            qs: {
+                nome: name
+            }
+        }).then(res => {
+            return res.body[0].id
+        })
+    })
+})
