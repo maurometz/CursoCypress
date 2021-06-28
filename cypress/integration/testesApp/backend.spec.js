fff1f3d5253b064c39ed7ed574b42d7b6e47ddf9
Mauro <maurometzdasilva@ienh.com.br>
2021-06-28 17:16:51 -0300

    Achou que eu tava brincando?

--- cypress/integration/testesApp/backend.spec.js
+++ cypress/integration/testesApp/backend.spec.js
@@ -2,44 +2,52 @@
 
 
 describe('Testes com o Barriga React', () => {
-
+    let token
     before(() => {
-        // cy.login('ehre@ehre.com', '123456789')
+        cy.getToken('ehre@ehre.com', '123456789')
+            .then(tkn => {
+                token = tkn
+            })
     })
-    
+
     beforeEach(() => {
-        // cy.resetApp()
+        cy.resetRest()
     })
 
     it('Adicionando conta', () => {
-       cy.request({
-           method: 'POST',
-           url: 'https://barrigarest.wcaquino.me/signin',
-           body: {
-               email: "ehre@ehre.com",
-               redirecionar: false,
-               senha: '123456789'
-           }
-       }).its('body.token').should('not.be.empty')
+        cy.request({
+            url: '/contas',
+            method: 'POST',
+            headers: { Authorization: `JWT ${token}` },
+            body: {
+                nome: 'Conta via rest'
+            }
+        }).as('response')
+
+        cy.get('@response').then(res => {
+            expect(res.status).to.be.equal(201)
+            expect(res.body).to.have.property('id')
+            expect(res.body).to.have.property('nome', 'Conta via rest')
+        })
     })
 
     it('Alterando conta', () => {
-       
+
     })
 
     it('Não deve criar uma conta com mesmo nome', () => {
-       
+
     })
 
-    it('Deve criar uma transação',() => {
-        
+    it('Deve criar uma transação', () => {
+
     })
 
     it('Pegar o saldo', () => {
-        
+
     })
 
     it('Deve remover um movimentação', () => {
-      
+
     })
 })
