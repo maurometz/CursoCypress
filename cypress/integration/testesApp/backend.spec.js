8bba8ee1f85e099903fac142c5aa55bebe9e45e3
Mauro <maurometzdasilva@ienh.com.br>
2021-06-28 17:37:43 -0300

    Upando a proibição de criar contas iguais

--- cypress/integration/testesApp/backend.spec.js
+++ cypress/integration/testesApp/backend.spec.js
@@ -32,11 +32,43 @@ describe('Testes com o Barriga React', () => {
     })
 
     it('Alterando conta', () => {
+        cy.request({
+            method: 'GET',
+            url: '/contas',
+            headers: { Authorization: `JWT ${token}` },
+            qs: {
+                nome: 'Conta para alterar'
+            }
+        }).then(res => {
+            cy.request({
+                url: `/contas/${res.body[0].id}`,
+                method: 'PUT',
+                headers: { Authorization: `JWT ${token}` },
+                body: {
+                    nome: 'conta alterada via rest'
+                }
+            }).as('response')
+        })
 
+        cy.get('@response').its('status').should('be.equal', 200)
     })
 
     it('Não deve criar uma conta com mesmo nome', () => {
+        cy.request({
+            url: '/contas',
+            method: 'POST',
+            headers: { Authorization: `JWT ${token}` },
+            body: {
+                nome: 'Conta mesmo nome'
+            },
+            failOnStatusCode: false
+        }).as('response')
 
+        cy.get('@response').then(res => {
+            console.log(res)
+            expect(res.status).to.be.equal(400)
+            expect(res.body.error).to.be.equal('Já existe uma conta com esse nome!')
+        })
     })
 
     it('Deve criar uma transação', () => {
