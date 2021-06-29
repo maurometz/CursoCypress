f11c22da55994d73156f69771820a621d71288cb
Mauro <maurometzdasilva@ienh.com.br>
2021-06-29 11:18:23 -0300

    Testes prontos o bonitos

--- cypress/integration/testesApp/frontend.spec.js
+++ cypress/integration/testesApp/frontend.spec.js
@@ -43,14 +43,23 @@ describe('Testes com o Barriga React', () => {
     it('Alterando conta', () => {
 
         cy.route({
+            method: 'GET',
+            url: '/contas/**',
+            response: [
+                {id:1, nome:'Carteira',visivel:true, usuario_id:1},
+                {id:2, nome:'Banco',visivel:true, usuario_id:1}
+        ]
+        }).as('contas')
+
+        cy.route({
             method: 'PUT',
             url: '/contas/**',
-            response: {id:1, nome:'First Account',visivel:true, usuario_id:1}
+            response: {id:1, nome:'Conta alterada',visivel:true, usuario_id:1}
         })
 
         cy.acessarMenuConta()
         .wait(1000)
-        cy.xpath(loc.CONTAS.FN_XP_BTN_ALTERAR('Carteira')).click().click()
+        cy.xpath(loc.CONTAS.FN_XP_BTN_ALTERAR('Carteira')).click()
         .wait(1000)
         cy.get(loc.CONTAS.NOME)
             .clear()
@@ -61,7 +70,7 @@ describe('Testes com o Barriga React', () => {
         .wait(1000)
     })
 
-    it.only('Não deve criar uma conta com mesmo nome', () => {
+    it('Não deve criar uma conta com mesmo nome', () => {
         cy.route({
             method: 'POST',
             url: '/contas',
@@ -77,23 +86,79 @@ describe('Testes com o Barriga React', () => {
     })
 
     it('Deve criar uma transação',() => {
+        cy.route({
+            method: 'POST',
+            url: '/transacoes',
+            response: {"id":616546,"descricao":"Umbrella Corps.","envolvido":"Jill Valentine","observacao":null,"tipo":"REC","data_transacao":"2021-06-29T03:00:00.000Z","data_pagamento":"2021-06-29T03:00:00.000Z","valor":"232.00","status":false,"conta_id":664975,"usuario_id":22325,"transferencia_id":null,"parcelamento_id":null}
+        })
+
+        cy.route({
+            method: 'GET',
+            url: '/extrato/**',
+            response: 'fixture:movimentacaoSalva'
+        })
+
         cy.get(loc.MENU.MOVIMENTACAO).click()
             
         cy.get(loc.MOVIMENTACAO.DESCRICAO).type('Desc')
         cy.get(loc.MOVIMENTACAO.VALOR).type('123')
         cy.get(loc.MOVIMENTACAO.INTERESSADO).type('Ethan Winters')
-        cy.get(loc.MOVIMENTACAO.CONTA).select('Conta com movimentacao')
+        cy.get(loc.MOVIMENTACAO.CONTA).select('Banco')
         cy.get(loc.MOVIMENTACAO.STATUS).click()
         cy.get(loc.MOVIMENTACAO.BTN_SALVAR).click()
         cy.get(loc.MESSAGE).should('contain', 'sucesso')
+
+        
 
         cy.get(loc.EXTRATO.LINHAS).should('have.length', 7)
         cy.xpath(loc.EXTRATO.FN_XP_BUSCA_ELEMENTO('Desc', '123')).should('exist')
     })
 
-    it('Pegar o saldo', () => {
+    it  ('Pegar o saldo', () => {
+        cy.route({
+            method: 'GET',
+            url: '/transacoes/**',
+            response:  {
+                "conta": "Conta para saldo",
+                "id": 616134,
+                "descricao": "Movimentacao 1, calculo saldo",
+                "envolvido": "CCC",
+                "observacao": null,
+                "tipo": "REC",
+                "data_transacao": "2021-06-29T03:00:00.000Z",
+                "data_pagamento": "2021-06-29T03:00:00.000Z",
+                "valor": "3500.00",
+                "status": true,
+                "conta_id": 664979,
+                "usuario_id": 22325,
+                "transferencia_id": null,
+                "parcelamento_id": null
+              }
+        })
+
+        cy.route({
+            method: 'PUT',
+            url: '/transacoes/**',
+            response:  {
+                "conta": "Conta para saldo",
+                "id": 616134,
+                "descricao": "Movimentacao 1, calculo saldo",
+                "envolvido": "CCC",
+                "observacao": null,
+                "tipo": "REC",
+                "data_transacao": "2021-06-29T03:00:00.000Z",
+                "data_pagamento": "2021-06-29T03:00:00.000Z",
+                "valor": "3500.00",
+                "status": true,
+                "conta_id": 664979,
+                "usuario_id": 22325,
+                "transferencia_id": null,
+                "parcelamento_id": null
+              }
+        })
+        
         cy.get(loc.MENU.HOME).click()
-        cy.xpath(loc.SALDO.FN_XP_SALDO_CONTA('Conta para saldo')).should('contain', '534,00')
+        cy.xpath(loc.SALDO.FN_XP_SALDO_CONTA('Carteira')).should('contain', '100,00')
 
         cy.get(loc.EXTRATO.MENU_EXTRATO).click()
         cy.xpath(loc.EXTRATO.FN_XP_ALTERAR_ELEMENTO('Movimentacao 1, calculo saldo')).click()
@@ -101,13 +166,34 @@ describe('Testes com o Barriga React', () => {
         cy.get(loc.MOVIMENTACAO.STATUS).click()
         cy.get(loc.MOVIMENTACAO.BTN_SALVAR).click()
         cy.get(loc.MESSAGE).should('contain', 'sucesso')
+
+        cy.route({
+            method: 'GET',
+            url: '/saldo',
+            response: [{
+                conta_id: 9999,
+                conta:"Carteira",
+                saldo:"4034.00"
+            },
+            {
+                conta_id: 2000,
+                conta:"Banco",
+                saldo:"1000000000.00"}
+            ]
+        }).as('saldoFinal')
         cy.wait(2000)
 
         cy.get(loc.MENU.HOME).click()
-        cy.xpath(loc.SALDO.FN_XP_SALDO_CONTA('Conta para saldo')).should('contain', '4.034,00')
+        cy.xpath(loc.SALDO.FN_XP_SALDO_CONTA('Carteira')).should('contain', '4.034,00')
     })
 
     it('Deve remover um movimentação', () => {
+        cy.route({
+            method: 'DELETE',
+            url: '/transacoes/**',
+            response: {},
+            status: 204
+        }).as('del')
         cy.get(loc.EXTRATO.MENU_EXTRATO).click()
         cy.xpath(loc.EXTRATO.FN_XP_REMOVER_ELEMENTO('Movimentacao para exclusao')).click()
         cy.get(loc.MESSAGE).should('contain', 'sucesso')
--- cypress/support/buildEnv.js
+++ cypress/support/buildEnv.js
@@ -33,6 +33,19 @@ const buildEnv = () => {
                 {id:2,nome:'Banco',visivel:true,usuario_id:1}
             ]
         }).as('contas')
+
+        cy.route({
+            method: 'GET',
+            url: '/extrato/**',
+            response:[
+                {"conta":"Conta para movimentacoes","id":616132,"descricao":"Movimentacao para exclusao","envolvido":"AAA","observacao":null,"tipo":"DESP","data_transacao":"2021-06-29T03:00:00.000Z","data_pagamento":"2021-06-29T03:00:00.000Z","valor":"-1500.00","status":true,"conta_id":664977,"usuario_id":22325,"transferencia_id":null,"parcelamento_id":null},
+                {"conta":"Conta com movimentacao","id":616133,"descricao":"Movimentacao de conta","envolvido":"BBB","observacao":null,"tipo":"DESP","data_transacao":"2021-06-29T03:00:00.000Z","data_pagamento":"2021-06-29T03:00:00.000Z","valor":"-1500.00","status":true,"conta_id":664978,"usuario_id":22325,"transferencia_id":null,"parcelamento_id":null},
+                {"conta":"Conta para saldo","id":616135,"descricao":"Movimentacao 2, calculo saldo","envolvido":"DDD","observacao":null,"tipo":"DESP","data_transacao":"2021-06-29T03:00:00.000Z","data_pagamento":"2021-06-29T03:00:00.000Z","valor":"-1000.00","status":true,"conta_id":664979,"usuario_id":22325,"transferencia_id":null,"parcelamento_id":null},
+                {"conta":"Conta para saldo","id":616136,"descricao":"Movimentacao 3, calculo saldo","envolvido":"EEE","observacao":null,"tipo":"REC","data_transacao":"2021-06-29T03:00:00.000Z","data_pagamento":"2021-06-29T03:00:00.000Z","valor":"1534.00","status":true,"conta_id":664979,"usuario_id":22325,"transferencia_id":null,"parcelamento_id":null},
+                {"conta":"Conta para extrato","id":616137,"descricao":"Movimentacao para extrato","envolvido":"FFF","observacao":null,"tipo":"DESP","data_transacao":"2021-06-29T03:00:00.000Z","data_pagamento":"2021-06-29T03:00:00.000Z","valor":"-220.00","status":true,"conta_id":664980,"usuario_id":22325,"transferencia_id":null,"parcelamento_id":null},
+                {"conta":"Conta para saldo","id":616134,"descricao":"Movimentacao 1, calculo saldo","envolvido":"CCC","observacao":null,"tipo":"REC","data_transacao":"2021-06-29T03:00:00.000Z","data_pagamento":"2021-06-29T03:00:00.000Z","valor":"3500.00","status":true,"conta_id":664979,"usuario_id":22325,"transferencia_id":null,"parcelamento_id":null},
+                {"conta":"Conta para alterar","id":616546,"descricao":"Umbrella Corps.","envolvido":"Jill Valentine","observacao":null,"tipo":"REC","data_transacao":"2021-06-29T03:00:00.000Z","data_pagamento":"2021-06-29T03:00:00.000Z","valor":"232.00","status":false,"conta_id":664975,"usuario_id":22325,"transferencia_id":null,"parcelamento_id":null}]
+        })
 }
 
 export default buildEnv
